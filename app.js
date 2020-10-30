068124aba778f478704111d70508eaab9879fb0f
wangshilin21 <wangshilin21@126.com>
2020-10-30 16:32:23 +0800

    1030

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ App({
   },
 
   globalData: {
-   carType:'VW48X',
+   carType:'NULL',
    keyID:0,//当前使用的诊断头ID
    ecuID:'none',//当前操作的ECU名称
    ecuList:[],//返回该台车的控制器列表
    chooseFunc:0,
-   globalUrl:'http://192.168.43.90:8080/CloudUDS/uds'
+   globalUrl:'http://192.168.31.177:8080/CloudUDS/uds'
   }
 });
